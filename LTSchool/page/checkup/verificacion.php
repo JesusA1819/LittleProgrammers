8ae293402ba9aa21cc68a7a7555c4ccd6dbcc870
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-12 15:56:34 -0600

    Update verificacion.php

--- LTSchool/page/checkup/verificacion.php
+++ LTSchool/page/checkup/verificacion.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-$servername = "lps1819.mariadb.database.azure.com";
-   $username = "JesusAlonso@lps1819";
-   $password = "Alonsogomez1819";
-   $database = "lpstec";
-
-   $conn = mysqli_connect($servername, $username, $password, $database);
+            $servername = "lps1819.mariadb.database.azure.com";
+            $username = "JesusAlonso@lps1819";
+            $password = "Alonsogomez1819";
+            $database = "lpstec";
+            
+            $conexion = mysqli_connect($servername, $username, $password, $database);
 if (!$conexion) {
     echo "No se realizó la conexión a la base de datos, el error fue:" . mysqli_connect_error();
 } else {
