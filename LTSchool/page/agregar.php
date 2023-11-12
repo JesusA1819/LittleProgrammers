d7a324f0376fe7a39bcda5e0910cbfa2d4b14bf2
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-12 15:46:34 -0600

    Update agregar.php

--- LTSchool/page/agregar.php
+++ LTSchool/page/agregar.php
@@ -8,12 +8,12 @@
             </div>
 
             <?php 
-            $servername = "bpwy1dcsa1mcx5dsui0u-mysql.services.clever-cloud.com";
-            $username = "uzdw4zvdkutw5xtl";
-            $password = "g12r5yTL04okkU0RTAJ5";
-            $database = "bpwy1dcsa1mcx5dsui0u";
-            
-            $conexion = mysqli_connect($servername, $username, $password, $database);
+            $servername = "lps1819.mariadb.database.azure.com";
+   $username = "JesusAlonso@lps1819";
+   $password = "Alonsogomez1819";
+   $database = "lpstec";
+
+   $conn = mysqli_connect($servername, $username, $password, $database);
             if (!$conexion) {
                 die("Error al conectar a la base de datos: " . mysqli_connect_error());
                 echo "Fallo de conexion vuelva despues";
