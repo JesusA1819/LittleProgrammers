cade92fc9d9d11d33711914667e5717c0da09b64
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-15 10:31:01 -0600

    Update home.php

--- LTSchool/page/home.php
+++ LTSchool/page/home.php
@@ -21,7 +21,7 @@ $matricula = $_SESSION['Matricula'];
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="../assets/css/estilos.css">
 
     <script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
