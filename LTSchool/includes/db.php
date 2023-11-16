8634bf04a36cb8e2bee464ee13bf54781649c8fc
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-15 21:36:46 -0600

    Update db.php

--- LTSchool/includes/db.php
+++ LTSchool/includes/db.php
@@ -1,15 +1,16 @@
 <?php
 
-    $servername = "lps1819.mariadb.database.azure.com";
+   $servername = "lps1819.mariadb.database.azure.com";
    $username = "JesusAlonso@lps1819";
    $password = "Alonsogomez1819";
    $database = "lpstec";
 
    $conexion = mysqli_connect($servername, $username, $password, $database);
     if(!$conexion){
-        echo "No se realizo la conexion a la basa de datos, el error fue:".
-        mysqli_connect_error() ;
-        header("Location: home.php");
+        echo "<script language='JavaScript'>
+        alert('Error de conexion.');
+        location.assign('../page/index.html');
+        </script>";
         exit();
     }
 
