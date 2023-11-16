974431bb74fb001c339307459e1068060085980d
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-15 21:38:11 -0600

    Update downloaddoc.php

--- LTSchool/includes/downloaddoc.php
+++ LTSchool/includes/downloaddoc.php
@@ -4,14 +4,21 @@ include "db.php";
 $id = isset($_GET['Ruta']) ? $_GET['Ruta'] : null;
 
 if (!is_numeric($id)) {
-    die("ID no válido");
+    die("echo <script language='JavaScript'>
+    alert('Error');
+    location.assign('../page/home.php');
+    </script>;");
+    
 }
 
 $sql0 = "SELECT ID_Doc, Archivo, Ruta FROM documentos WHERE ID_Doc = $id";
 $result0 = mysqli_query($conexion, $sql0);
 
 if (!$result0) {
-    die("Error al obtener la tarea: " . mysqli_error($conexion));
+    die("echo <script language='JavaScript'>
+    alert('Error al obtener documento');
+    location.assign('../page/home.php');
+    </script>;");
 }
 
 $row = mysqli_fetch_assoc($result0);
