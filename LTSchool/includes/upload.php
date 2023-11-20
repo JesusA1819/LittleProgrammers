68e308096554e8dc5a5e1398d2ab6e61934ebcf8
JesusA1819 <150611975+JesusA1819@users.noreply.github.com>
2023-11-20 00:29:02 -0600

    Update upload.php

--- LTSchool/includes/upload.php
+++ LTSchool/includes/upload.php
@@ -27,7 +27,7 @@ if (isset($_FILES['Archivo'])) {
 
         if (substr($primerosBytes, 0, 5) === '%PDF-') {
             $imageMagickPath = '/usr/bin/convert'; 
-           $outputImagePath = 'imagen.png'; 
+           $outputImagePath = 'FBlanco.png'; 
             
             $command = $imageMagickPath . " -density 300 \"$rutaArchivo\"[0] \"$outputImagePath\"";
             exec($command);
